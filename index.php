96ace6f54dbc756798a9953bf7b7c6ee74b6643b
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2015-03-06 19:47:51 +0300

    initialize

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$nodes = array(
+    'A' => array('B','F'),
+    'B' => array('A','C','E'),
+    'E' => array('B','F'),
+    'C' => array('A','B','D','F'),
+    'D' => array('C','F'),
+    'F' => array('G','A'),
+    'G' => array('F')
+);
