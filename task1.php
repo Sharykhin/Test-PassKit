467bad642ff48360a5cb0b50914f8317f6a1cbe7
chapal <chapal@inbox.ru>
2015-03-08 14:26:29 +0300

    The implementation of first task has been changed

--- task1.php
+++ task1.php
@@ -4,48 +4,37 @@
  */
 
 
-$strings = array( 
-   "PassKit", 
-   "Fun", 
-   "Hong Kong", 
-   "Awesome", 
-   "PFR", 
-   "Mobile", 
-   "Office"
+$strings = array(
+    "PassKit",
+    "Fun",
+    "Hong Kong",
+    "Awesome",
+    "PFR",
+    "Mobile",
+    "Office"
 );
 
-/**
- * Function finds the shortest and the longest items in array
- *
- * @param array $strings
- * @return array
- */
 function getLongestAndShortestItems(array $strings) {
-    // Initialzie variables which are responsible for the longest and the shortest items
-    $searchLenght = 0;
+    // Initialize variables which are responsible for the longest and the shortest items
+    $convertToNumbers = array();
     $searchLongestItem = null;
     $searchShortestItem = null;
 
-    //Make array_walk to find the longest item comparing each item with each other
-    array_walk($strings,function($item) use (&$searchLenght,&$searchLongestItem){
-        if (strlen($item) > $searchLenght) {
-            $searchLenght = strlen($item);
-            $searchLongestItem = $item;
-        }
+    //Fill the new array by length of each item
+    array_walk($strings,function($item) use (&$convertToNumbers){
+        $convertToNumbers[] = strlen($item);
     });
 
-    // Make arra_walk again to find the shortest item. It depends on the longest item
-    array_walk($strings,function($item) use (&$searchLenght,&$searchShortestItem){
-        if (strlen($item) < $searchLenght) {
-            $searchLenght = strlen($item);
-            $searchShortestItem = $item;
-        }
-    });
+    $searchLongestItem = $strings[array_keys($convertToNumbers,max($convertToNumbers))[0]];
+    $searchShortestItem = $strings[array_keys($convertToNumbers,min($convertToNumbers))[0]];
+
+
 
     return array(
-            'longest' =>$searchLongestItem,
-            'shortest' => $searchShortestItem
+        'longest' =>$searchLongestItem,
+        'shortest' => $searchShortestItem
     );
+
 }
 
 var_dump(getLongestAndShortestItems($strings));
